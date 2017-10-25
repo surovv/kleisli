35118b8038f0be9af51ee4bc1e1555b1bbeb95fc
surov <brainsurov@gmail.com>
2017-10-25 16:19:25 +0300

    Rewrite Functor with typing
    AWW HERE IT GOES

--- src/lib/functor.js
+++ src/lib/functor.js
@@ -1,3 +1,24 @@
-export default class Functor {
-  fmap(fn) { throw new Error('fmap must be implemented') }
-}
+import createType, {addPrefixNotationMethods} from './typing';
+
+
+const type = class Functor {};
+const mixins = [];
+
+const instanceMethods = (...args) => ({
+  fmap: fn => { throw new Error('fmap must be implemented'); },
+});
+
+const FunctorInstance = createType(type, mixins, instanceMethods)
+
+const prefixNotationMethods = {
+  unit: FunctorInstance,
+
+  fmap: (fn, fa) => fa.fmap(fn)
+};
+
+
+export const Functor = addPrefixNotationMethods(FunctorInstance, prefixNotationMethods);
+
+
+
+export default Functor;
