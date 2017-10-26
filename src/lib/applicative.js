ab0d7791ba30397ab42dd126f86bddcc680de8e3
surov <brainsurov@gmail.com>
2017-10-26 16:20:15 +0300

    Rewrite Applicative with typing

--- src/lib/applicative.js
+++ src/lib/applicative.js
@@ -1,5 +1,28 @@
+import createType, {addPrefixNotationMethods} from './typing';
 import Functor from './functor';
 
-export default class Applicative extends Functor {
-  apply(ffn) { throw new Error('apply must be implemented') }
-}
+
+const type = class Applicative {};
+const mixins = [Functor];
+
+const instanceMethods = (...args) => ({
+  ap: ffn => ffn.fmap(fn => fn(...args)), //apply
+  lift: (fn, ...fargs) => fargs.reduce((fCrrdFn, fb) => fb.apply(fCrrdFn), curry(fn)(...args)),
+});
+
+const ApplicativeInstance = createType(type, mixins, instanceMethods);
+
+
+const prefixNotationMethods = {
+  unit: ApplicativeInstance,
+
+  ap: (ffn, fa) => fa.ap(ffn),
+  lift: (fn, fa, ...fargs) => fa.lift(fn, ...fargs),
+};
+
+
+export const Applicative = addPrefixNotationMethods(ApplicativeInstance, prefixNotationMethods);
+
+
+
+export default Applicative;
