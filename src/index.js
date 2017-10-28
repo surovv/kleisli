4ac1805ccab49aff6590c626804472a78fc8690e
surov <brainsurov@gmail.com>
2017-10-28 17:37:18 +0300

    Rewrite Monad with Typing

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ export * as F from './lib/fn';
 //export monadenize from './lib/monadenize'
 export Functor from './lib/functor';
 export Applicative from './lib/applicative';
-//export Monad from './lib/monad';
+export Monad from './lib/monad';
 //export Identity from './lib/identity';
 //export Maybe, {Just, Nothing} from './lib/maybe';
--- src/lib/monad.js
+++ src/lib/monad.js
@@ -1,9 +1,36 @@
+import createType, {addPrefixNotationMethods} from './typing';
 import Applicative from './applicative';
 
-export default class Monad extends Applicative{
-  bind(fn) { throw new Error('bind must be implemented') }
-  sbind(fn) { throw new Error('sbind must be implemented') }
-  compose(fn) { throw new Error('compose must be implemented') }
-  join(val){ throw new Error('join must be implemented') }
-  fail(str){ throw new Error('fail must be implemented') }
-}
+
+const type = class Monad {};
+const mixins = [Applicative];
+
+const instanceMethods = (...args) => ({
+  bind: fn => fn(...args),
+  seq: fn => fn(),
+
+//  is implied that args = [ma], and join:: calling on m(ma), so join :: m(m a) = m a
+//  it could have type checking like
+//  (args.length === 1 && args[0].bind) ?
+//      args[0].bind(a => a.constructor.name === args[0].constructor.name ? a : new Error('types error')) : new Error('types error');
+  join: () => args[0],
+
+//  fail: message => new Error(str),
+});
+
+const MonadInstance = createType(type, mixins, instanceMethods);
+
+const prefixNotationMethods = {
+  pure: MonadInstance,
+
+  bind: (ma, fn) => ma.bind(fn),
+  seq: (ma, fn) => ma.seq(fn),
+  join: mma => mma.join(),
+//  fail: (ma, str) => ma.fail(str),
+};
+
+
+export const Monad = addPrefixNotationMethods(MonadInstance, prefixNotationMethods);
+
+
+export default Monad;
