f7398eb9d18feceb01b3d70ade2c9a9e9abbe82d
surov <brainsurov@gmail.com>
2018-03-25 14:40:34 +0300

    Update fn module

--- src/lib/fn.js
+++ src/lib/fn.js
@@ -1,8 +1,15 @@
+/* eslint-disable fp/no-mutating-methods */
 export const flip = (fn, args) => fn([...args].reverse());
+/* eslint-enable fp/no-mutating-methods */
 
-export const compose = fns => fns.reduce((f,g) => args => f(g(...args)));
+// call fns from last to first
+export const compose = fns => fns.reduce((g, f) => (...args) => g(f(...args)));
 export const composeLeft = fns => flip(compose, fns);
 
 export const apply = (fn, args) => fn(...args);
 
-export const curry = (fn, ...args) => args.length === fn.length ? fn(...args) : (...nextArgs) => curry(fn, ...args, ...nextArgs);
+export const curry = (fn, ...args) => args.length === fn.length ?
+  fn(...args) :
+  (...nextArgs) => curry(fn, ...args, ...nextArgs);
+
+export const later = fns => args => () => compose(fns)(args);
