01239aedf38711cde8a6f796e2b165671b46a5ed
surov <brainsurov@gmail.com>
2018-03-28 15:25:11 +0300

    Add compose method and annotations to Monad

--- src/lib/monad.js
+++ src/lib/monad.js
@@ -6,17 +6,25 @@ const Type = class Monad {};
 const mixins = [Applicative];
 
 const constructor = (...args) => ({
+  // bind :: (args -> mb) -> mb
+  // (>>=) :: ma -> (a -> mb) -> mb
   bind: f => f(...args),
+  // seq :: (() -> ma) -> ma
+  // mv1 >> mv2 = mv1 >>= (\_ -> mv2)
   seq: f => f(),
+  // compose :: args -> mb, b -> mc, ..., d -> me) -> me)
+  compose: (f, ...fns) => fns.reduce(
+    (mb, g) => mb.bind(g),
+    f(...args)
+  ),
 
-  //  is implied that args = [ma], and join:: calling on m(ma), so join :: m(m a) -> m a
-  //  it could have type checking like
-  //  (args.length === 1 && args[0].bind) ?
-  //      args[0].bind(a => a.constructor.name === args[0].constructor.name ?
-  //      a : new Error('types error')) : new Error('types error');
+  // join :: () -> ma
+  // is implied that args = [ma], and join is calling on m(ma), so join :: m(m a) -> m a
+  // maybe it could have type checking in future, for example
+  // (args.length === 1 && args[0].bind) ?
+  //   args[0].bind(a => a.constructor.name === args[0].constructor.name ?
+  //   a : new Error('types error')) : new Error('types error');
   join: () => args[0],
-
-//  fail: message => new Error(str),
 });
 
 
@@ -24,7 +32,6 @@ const staticMethods = {
   bind: (ma, f) => ma.bind(f),
   seq: (ma, f) => ma.seq(f),
   join: mma => mma.join(),
-  //  fail: (ma, str) => ma.fail(str),
 };
 
 
