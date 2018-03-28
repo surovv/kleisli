6c323d346b6e77fc45a7c0043896b5fbd4e6d51b
surov <brainsurov@gmail.com>
2018-03-28 15:12:05 +0300

    Add annotation for functions in fn module
    Upddate flip fn

--- src/lib/fn.js
+++ src/lib/fn.js
@@ -1,15 +1,26 @@
 /* eslint-disable fp/no-mutating-methods */
-export const flip = (fn, args) => fn([...args].reverse());
+// flip :: (* -> d) -> * -> d
+// flip :: ((a, b, c) -> d) -> (c, b, a) -> d
+export const flip = fn => (...args) => fn([...args].reverse());
 /* eslint-enable fp/no-mutating-methods */
 
-// call fns from last to first
-export const compose = fns => fns.reduce((g, f) => (...args) => g(f(...args)));
-export const composeLeft = fns => flip(compose, fns);
+// compose :: (c -> d, ..., a -> b) -> a -> d
+// (.) :: (b -> c) -> (a -> b) -> (a -> c)
+export const compose = (...fns) => fns.reduce(
+  (g, f) => (...args) => g(f(...args))
+);
+// composeLeft :: (a -> b, ..., c -> d) -> a -> d
+// (>.>) :: (a -> b) -> (b -> c) -> (a -> c)
+export const composeLeft = flip(compose);
 
+// apply :: (a -> b, [a]) -> b
+// ($) :: (a -> b) -> a -> b
 export const apply = (fn, args) => fn(...args);
 
+// curry :: ((a, ..., c) -> d) -> (a -> ... -> c -> d)
 export const curry = (fn, ...args) => args.length === fn.length ?
   fn(...args) :
   (...nextArgs) => curry(fn, ...args, ...nextArgs);
 
-export const later = fns => args => () => compose(fns)(args);
+// later :: (a -> b, ..., c -> d) -> a -> () -> d
+export const later = (...fns) => args => () => composeLeft(fns)(args);
