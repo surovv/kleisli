4e9b6dd515408036bdb46840564c3470fc57e4f8
Sergey Zhulin <brainsurov@gmail.com>
2018-12-24 23:35:47 +0200

    Remove haskell annotations from fn module

--- src/lib/fn.js
+++ src/lib/fn.js
@@ -1,26 +1,23 @@
-/* eslint-disable fp/no-mutating-methods */
-// flip :: (* -> d) -> * -> d
 // flip :: ((a, b, c) -> d) -> (c, b, a) -> d
+/* eslint-disable-next-line fp/no-mutating-methods */
 export const flip = fn => (...args) => fn([...args].reverse());
-/* eslint-enable fp/no-mutating-methods */
 
 // compose :: (c -> d, ..., a -> b) -> a -> d
-// (.) :: (b -> c) -> (a -> b) -> (a -> c)
 export const compose = (...fns) => fns.reduce(
-  (g, f) => (...args) => g(f(...args))
+  (g, f) => (...args) => g(f(...args)),
 );
 // composeLeft :: (a -> b, ..., c -> d) -> a -> d
-// (>.>) :: (a -> b) -> (b -> c) -> (a -> c)
 export const composeLeft = flip(compose);
 
 // apply :: (a -> b, [a]) -> b
-// ($) :: (a -> b) -> a -> b
 export const apply = (fn, args) => fn(...args);
 
 // curry :: ((a, ..., c) -> d) -> (a -> ... -> c -> d)
-export const curry = (fn, ...args) => args.length === fn.length ?
-  fn(...args) :
-  (...nextArgs) => curry(fn, ...args, ...nextArgs);
+export const curry = (fn, ...args) => (
+  args.length === fn.length ?
+    fn(...args) :
+    (...nextArgs) => curry(fn, ...args, ...nextArgs)
+);
 
 // later :: (a -> b, ..., c -> d) -> a -> () -> d
 export const later = (...fns) => args => () => composeLeft(fns)(args);
